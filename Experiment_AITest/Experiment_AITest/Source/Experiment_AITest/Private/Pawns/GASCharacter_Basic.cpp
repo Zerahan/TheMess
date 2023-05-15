765e369ebf20b8f07ba886f55d9de2aa9af3ded6
James Isbell <jisbell01@gmail.com>
2023-05-15 10:49:10 -0700

    AITest: Engine update

---
+++ Experiment_AITest/Experiment_AITest/Source/Experiment_AITest/Private/Pawns/GASCharacter_Basic.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pawns/GASCharacter_Basic.h"
+
+// Sets default values
+AGASCharacter_Basic::AGASCharacter_Basic()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	GASComponent = CreateDefaultSubobject<UAbilitySystemComponent_Basic>(TEXT("GAS Component"));
+}
+
+// Called when the game starts or when spawned
+void AGASCharacter_Basic::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGASCharacter_Basic::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AGASCharacter_Basic::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Experiment_AITest/Experiment_AITest/Source/Experiment_AITest/Public/Pawns/GASCharacter_Basic.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "GASCharacter_Basic.generated.h"
+
+UCLASS()
+class EXPERIMENT_AITEST_API AGASCharacter_Basic : public ACharacter
+{
+	GENERATED_BODY()
+
+	class UAbilitySystemComponent_Basic* GASComponent;
+
+public:
+	// Sets default values for this character's properties
+	AGASCharacter_Basic();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
